b464a68f368324229b9ed77d7c8a2199f6d16cf4
Zachary Wasserman <zachwass2000@gmail.com>
2012-12-07 14:46:40 -0500

    Basic REPL

--- Main.hs
+++ Main.hs
@@ -1,10 +1,38 @@
 module Main where
+
+import Data.Map as M
+
 import Text.Parsec
+import System.Console.Shell
+import System.Console.Shell.ShellMonad
+import System.Console.Shell.Backend.Basic
+
 import SchemeParser
 import Scheme
 
-parseAndRun :: String -> Either String Val
-parseAndRun s = case parse parseExp "" s of
-  Left e -> Left $ show e
-  Right p -> eval p
+
+mkShDesc :: ShellDescription NS
+mkShDesc =
+  initialShellDescription
+       { prompt = \_ -> return "> "
+       , commandStyle = OnlyCommands
+       , historyEnabled = True
+       }
+
+initShState = M.empty
+
+shell = do
+    let
+      desc =
+         (mkShellDescription [] shellEval)
+         { greetingText       = Just ("Hello, shell")
+         , secondaryPrompt    = Just $ \_ -> return "] "
+         }
+    runShell desc basicBackend initShState
+
+shellEval :: String -> Sh NS ()
+shellEval s =
+  case parseAndRun s of
+    Left e -> shellPutErrLn e
+    Right p -> shellPutStrLn $ show p
   
